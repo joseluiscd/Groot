15f7c573f4a271f7109f5c56313cee660951cd9c
José Luis C.D <jlcd0003@red.ujaen.es>
2021-11-24 23:51:36 +0100

    Python imgui update

--- app/python_imgui.cpp
+++ app/python_imgui.cpp
@@ -9,86 +9,72 @@ void create_imgui_module()
 {
     using namespace boost::python;
 
-    class_<ImVec2>("ImVec2", init<float, float>())
+    scope current_scope = class_<ImGuiContext>("ImGui", no_init);
+
+    class_<ImVec2>("Vec2", init<float, float>())
         .def(init<>())
         .def_readwrite("x", &ImVec2::x)
         .def_readwrite("y", &ImVec2::y);
 
-    class_<ImVec4>("ImVec4", init<float, float, float, float>())
+    class_<ImVec4>("Vec4", init<float, float, float, float>())
         .def(init<>())
         .def_readwrite("x", &ImVec4::x)
         .def_readwrite("y", &ImVec4::y)
         .def_readwrite("z", &ImVec4::z)
         .def_readwrite("w", &ImVec4::w);
 
-    class_<ImGuiContext>("ImGui", "ImGui context")
-        .def(
-            "Begin", +[](const char* name, bool closable, int flags) {
-                bool open = true;
+    def(
+        "Begin", +[](const char* name, bool closable, int flags) {
+            bool open = true;
 
-                return make_tuple(ImGui::Begin(name, closable ? &open : nullptr, flags), open);
-            },
-            (arg("name"), arg("closable") = false, arg("flags") = 0), "returns (expanded, opened)")
-        .staticmethod("Begin")
+            return make_tuple(ImGui::Begin(name, closable ? &open : nullptr, flags), open);
+        },
+        (arg("name"), arg("closable") = false, arg("flags") = 0), "returns (expanded, opened)");
 
-        .def("End", &ImGui::End)
-        .staticmethod("End")
+    def("End", &ImGui::End);
 
-        .def("Button", &ImGui::Button, (arg("text"), arg("size") = ImVec2(0, 0)))
-        .staticmethod("Button")
+    def("Button", &ImGui::Button, (arg("text"), arg("size") = ImVec2(0, 0)));
 
-        .def(
-            "Checkbox", +[](const char* label, bool state) {
-                return make_tuple(ImGui::Checkbox(label, &state), state);
-            },
-            (arg("label"), arg("state")), "returns (clicked, state)")
-        .staticmethod("Checkbox")
+    def(
+        "Checkbox", +[](const char* label, bool state) {
+            return make_tuple(ImGui::Checkbox(label, &state), state);
+        },
+        (arg("label"), arg("state")), "returns (clicked, state)");
 
-        .def(
-            "Text", +[](const char* text) { ImGui::Text("%s", text); }, (arg("text")))
-        .staticmethod("Text")
+    def(
+        "Text", +[](const char* text) { ImGui::Text("%s", text); }, (arg("text")));
 
-        .def(
-            "RadioButton", +[](const char* label, bool state) -> bool {
-                return ImGui::RadioButton(label, state);
-            },
-            (arg("label"), arg("active")))
-        .staticmethod("RadioButton")
+    def(
+        "RadioButton", +[](const char* label, bool state) -> bool {
+            return ImGui::RadioButton(label, state);
+        },
+        (arg("label"), arg("active")));
 
-        .def(
-            "Combo", +[](const char* label, int current, list items, int height_in_items) {
-                str in_items = str("\0").join(items);
+    def(
+        "Combo", +[](const char* label, int current, list items, int height_in_items) {
+            str in_items = str("\0").join(items);
 
-                return make_tuple(
-                    ImGui::Combo(label, &current, extract<const char*>(in_items), height_in_items),
-                    current);
-            },
-            (arg("label"), arg("current"), arg("items"), arg("height_in_items") = -1), "returns (changed, current)")
+            return make_tuple(
+                ImGui::Combo(label, &current, extract<const char*>(in_items), height_in_items),
+                current);
+        },
+        (arg("label"), arg("current"), arg("items"), arg("height_in_items") = -1), "returns (changed, current)");
 
-        .def("BeginMenuBar", &ImGui::BeginMenuBar)
-        .staticmethod("BeginMenuBar")
+    def("BeginMenuBar", &ImGui::BeginMenuBar);
 
-        .def("EndMenuBar", &ImGui::EndMenuBar)
-        .staticmethod("EndMenuBar")
+    def("EndMenuBar", &ImGui::EndMenuBar);
 
-        .def("BeginMenu", &ImGui::BeginMenu, (arg("label"), arg("enabled") = true))
-        .staticmethod("BeginMenu")
+    def("BeginMenu", &ImGui::BeginMenu, (arg("label"), arg("enabled") = true));
 
-        .def("EndMenu", &ImGui::EndMenu)
-        .staticmethod("EndMenu")
+    def("EndMenu", &ImGui::EndMenu);
 
-        .def(
-            "MenuItem", +[](const char* label, const char* shortcut, bool selected, bool enabled) {
-                return make_tuple(
-                    ImGui::MenuItem(label, shortcut, &selected, enabled),
-                    selected);
-            },
-            (arg("label"), arg("shortcut") = "", arg("selected") = false, arg("enabled") = true), "returns (clicked, state)")
-        .staticmethod("MenuItem")
+    def(
+        "MenuItem", +[](const char* label, const char* shortcut, bool selected, bool enabled) {
+            return make_tuple(
+                ImGui::MenuItem(label, shortcut, &selected, enabled),
+                selected);
+        },
+        (arg("label"), arg("shortcut") = "", arg("selected") = false, arg("enabled") = true), "returns (clicked, state)");
 
-        .def("Separator", &ImGui::Separator)
-        .staticmethod("Separator")
-
-        /**/
-        ;
+    def("Separator", &ImGui::Separator);
 }
